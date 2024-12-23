4a6fd8cfca37d447c1e9eb8c61dbda2e20cfc29b
Ankit Singhal <itforankit@gmail.com>
2024-12-23 08:32:44 +0530

    adding notebook folder and ankitml doc file

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+#import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -20,4 +21,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-     
+
+""" if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.error(e)
+        logging.info("Divided by zero error occurred")
+        raise CustomException(e,sys)    """  
